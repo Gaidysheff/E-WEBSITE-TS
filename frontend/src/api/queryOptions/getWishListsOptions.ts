ce9ed609e54c93c5e8c2f89f4f396389afaeaa54
Gaidysheff <gaidysheff@mail.ru>
2026-01-02 20:27:58 +0500

    AUTH done v.2

--- frontend/src/api/queryOptions/getWishListsOptions.ts
+++ frontend/src/api/queryOptions/getWishListsOptions.ts
@@ -1,24 +1,24 @@
-import { WISHLISTS_GET_URL } from "@/api/endpoints.js";
-import api from "@/api/api.js";
-import { queryOptions } from "@tanstack/react-query";
+// import { WISHLISTS_GET_URL } from "@/api/endpoints.js";
+// import api from "@/api/api.js";
+// import { queryOptions } from "@tanstack/react-query";
 
-const getWishListsOptions = (email) => {
-  return queryOptions({
-    queryKey: ["userWishLists", email],
-    queryFn: () => getWishListsAction(email),
-  });
-};
+// const getWishListsOptions = (email) => {
+//   return queryOptions({
+//     queryKey: ["userWishLists", email],
+//     queryFn: () => getWishListsAction(email),
+//   });
+// };
 
-const getWishListsAction = async (email) => {
-  if (email) {
-    // --------------- Fetching delay ----------------------
-    // await new Promise((resolve) => setTimeout(resolve, 4000));
-    // -----------------------------------------------------
-    const response = await api.get(`${WISHLISTS_GET_URL}${email}`);
-    return response.data;
-  } else {
-    return null;
-  }
-};
+// const getWishListsAction = async (email) => {
+//   if (email) {
+//     // --------------- Fetching delay ----------------------
+//     // await new Promise((resolve) => setTimeout(resolve, 4000));
+//     // -----------------------------------------------------
+//     const response = await api.get(`${WISHLISTS_GET_URL}${email}`);
+//     return response.data;
+//   } else {
+//     return null;
+//   }
+// };
 
-export default getWishListsOptions;
+// export default getWishListsOptions;
