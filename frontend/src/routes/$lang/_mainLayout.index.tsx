3840d9ac9bc2d93b772b4cbc3b349393b4df7d57
Gaidysheff <gaidysheff@mail.ru>
2026-07-25 23:10:58 +0500

    CORS check-8

--- frontend/src/routes/$lang/_mainLayout.index.tsx
+++ frontend/src/routes/$lang/_mainLayout.index.tsx
@@ -22,7 +22,7 @@ export const Route = createFileRoute("/$lang/_mainLayout/")({
         "https://e-shop-ts-back.onrender.com/api/products_for_carousel/",
       );
       // ВНИМАТЕЛЬНО посмотрите в консоль браузера на этот объект!
-      console.log("Данные от Django:", response.data);
+      console.log("Данные от Django:", response);
 
       // Временно возвращаем пустой массив, чтобы компонент не падал из-за .map()
       return [];
